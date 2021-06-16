a80f814fd573a16e0c34e9364ff86e3cdf0e0206
Akshayar281 <vakshayrao@gmail.com>
2021-06-16 21:56:00 +0530

    first commit

---
+++ insertionSort.py
@@ -0,0 +1,20 @@
+# Program to sorting in ascending order using Insertion Sort.
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i] # key to sort
+        j = i - 1  #previous value
+        while j >= 0 and key < arr[j]:
+            # Traversing sorted array, if found any value is less than key value , keep sorting
+            arr[j + 1] = arr[j]
+            j -= 1
+            print(arr, "insorted array")    
+        arr[j + 1] = key
+        print(arr, "sorted array")
+       
+arr = [ 9, 8, 7, 6, 4, 3, 2]
+insertionSort(arr)
+print(arr, "sorted array")
+
+
+        
+    
