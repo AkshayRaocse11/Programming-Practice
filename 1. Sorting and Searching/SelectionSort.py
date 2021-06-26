245fe42d60f4b53351e11367d2f8906a0da70122
Akshayar281 <vakshayrao@gmail.com>
2021-06-26 19:27:22 +0530

    selection sort

--- 1. Sorting and Searching/SelectionSort.py
+++ 1. Sorting and Searching/SelectionSort.py
@@ -1,9 +1,10 @@
 def selectionSort(arr):
     for i in range(len(arr)):
+        min = i
         for j in range(i+1,len(arr)):
-            if arr[j]<arr[i]:
-                arr[i],arr[j] = arr[j],arr[i]       
-
+            if arr[j]<arr[min]:
+                min = j       
+        arr[i],arr[min] = arr[min],arr[i]
 arr = [10,2,4,12,12,0]     
 selectionSort(arr)   
 print(arr,"Selection Sort")
