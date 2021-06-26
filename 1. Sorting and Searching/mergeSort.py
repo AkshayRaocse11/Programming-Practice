013027319f689554f4dd43ace207090375fca0c7
Akshayar281 <vakshayrao@gmail.com>
2021-06-26 11:16:15 +0530

    merge sort 2.0

--- 1. Sorting and Searching/mergeSort.py
+++ 1. Sorting and Searching/mergeSort.py
@@ -1,62 +1,35 @@
-import math
-
-#Program to merge sort
-
-def mergeSort(arr,l,r):
-    
-    if l < r:
-  
-        # Same as (l+r)//2, but avoids overflow for
-        # large l and h
-        m = (l+(r-1))//2
-  
-        # Sort first and second halves
-        mergeSort(arr, l, m)
-        mergeSort(arr, m+1, r)
-        merge(arr, l, m, r)
-        
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r- m 
-    # create temp arrays
-    L = [0] * (n1)
-    R = [0] * (n2)
-  
-    # Copy data to temp arrays L[] and R[]
-    for i in range(0 , n1):
-        L[i] = arr[l + i]
-  
-    for j in range(0 , n2):
-        R[j] = arr[m + 1 + j]    
- # Merge the temp arrays back into arr[l..r]
-    i = 0     # Initial index of first subarray
-    j = 0     # Initial index of second subarray
-    k = l     # Initial index of merged subarray
-  # copying temp array values into actual array but its not sorted properly
-    while i < n1 and j < n2 :
-        if L[i] <= R[j]:
-            arr[k] = L[i]
-            i += 1
+def mergeSort(arr):
+    if len(arr)>1:
+        mid = (len(arr))//2
+        left = arr[:mid]
+        right = arr[mid:] 
+        mergeSort(left)
+        mergeSort(right)
+        merge(arr,left,right)
+def merge(arr,left,right):
+    i = 0
+    j =0 
+    k = 0
+    while i<len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i+=1
         else:
-            arr[k] = R[j]
-            j += 1
-        k += 1
-    # Copy the remaining elements of L[], if there , eg array may contain duplicate elements
-    # are any
-    while i < n1:
-        arr[k] = L[i]
-        i += 1
-        k += 1
-    # Copy the remaining elements of R[], if there
-    # are any
-    while j < n2:
-        arr[k] = R[j]
-        j += 1
-        k += 1
+            arr[k] =right[j]
+            j+=1
+        k+=1      
+    while i < len(left):
+        arr[k] = left [i]
+        i+=1
+        k+=1
+    while j<len(right)    :
+        arr[k] = right[j]
+        j+=1
+        k+=1
+    
+arr = [12,1,2,5,6,9,8,7,4,2]    
+mergeSort(arr)    
+print(arr)
+#O(nlogn) TC and Sc O(n)
 
 
-
-arr = [ 9, 8, 7, 6, 4, 3, 2]
-n =len(arr)
-mergeSort(arr,0,n-1)
-print(arr,"merge sort")
