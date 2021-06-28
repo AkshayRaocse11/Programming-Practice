9924716109108fc15e46f768eb20ac1af30b8097
Akshayar281 <vakshayrao@gmail.com>
2021-06-29 00:14:46 +0530

    Majority element

---
+++ Array Problems/3. Find Majoriity of Element.py
@@ -0,0 +1,19 @@
+def countSort(arr):
+    n = len(arr)
+    max_ele = max(arr)
+    count = [0]*(max_ele+1)
+    seen = {}
+    # store the count of each element of count array
+    for i in range(n):
+        count[arr[i]] += 1
+        seen[arr[i]] = count[arr[i]]
+    all_values = seen.values()
+    max_value = max(all_values)
+    for k, v in seen.items():
+        if v == max_value:
+            if len(arr)/2 < max_value:
+                return k
+
+
+arr = [1, 2, 2, 3, 2, 2, 5, 5, 5, 5, 5, 5, 5]
+print(countSort(arr), "Majority elements")
