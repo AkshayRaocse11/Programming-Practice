e7c8d6871e3ccf3741774227c243ec6a51b0c565
AkshayRaocse11 <vakshayrao@gmail.com>
2021-11-22 19:45:39 +0530

    Stack

--- Data Structure/Stack/Stack.py
+++ Data Structure/Stack/Stack.py
@@ -1,18 +1,41 @@
-def removeDuplicates(str1):
-    stack = "abbacca"
-    holdArray = [] 
-    str1 = ""
-    for element in range(0, len(stack)):
-        holdArray.append(stack[element])
-    for element in range(1, len(holdArray)):
-        if holdArray[element-1]==holdArray[element]:
-            holdArray.pop(element)
-            holdArray.pop(element-1)
-            removeDuplicates(str1)    
+class Stack:
+    def __init__(self):
+        self.stack=[]
+    def isEmpty(self):
+        return self.stack ==  []
+    def push(self,ele):
+        self.stack.append(ele)
+    def pop(self):
+        if not self.isEmpty():
+            return self.stack.pop()
         else:
-            str1 += stack[element]
-    
-     
-    return holdArray
-stack = "abbacca"
-removeDuplicates(stack) 
+            return -1
+    def peek(self):
+        if not self.isEmpty():
+            return self.stack[-1]   
+s= Stack()     
+while True:
+    print("push")
+    print("pop")
+    print("peek")
+    do = input("What operation u want to perform")
+    if do == "push":
+        ele = int(input("Insert Element into Stack "))
+        s.push(ele)
+    elif do == 'pop' :
+        ele = s.pop()
+        if ele == -1:
+            print("Stack Empty")
+        else:
+            print("Deleted element from the stack is ={10}".format(ele))
+    elif do == 'peek':
+        ele = s.peek()
+        if ele == -1:
+            print("Stack Empty") 
+        else:
+            print("Top of the element from the stack is ={}".format(ele)) 
+    else:
+        break     
+
+
+
